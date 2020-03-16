4685c9d084e9d3605b07391f1d71b3adf88f7971
Michael Hernandez <gammabeta010@gmail.com>
2020-03-16 21:47:22 +0100

    One row with 6 blocks. Works with API call and changing colors.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,35 @@
 $(document).ready(function() {
 
+    $('.row .square').on('click', function(event) {
+
+        callAjax($(this));
+
+
+    })
+
+    function callAjax(currObj) {
+    $.ajax({
+        url : 'https://flynn.boolean.careers/exercises/api/random/int',
+        method : 'GET',
+        success: function(data) {
+            console.log(data.response);
+            currObj.html(data.response);
+
+            if(data.response <= 5) {
+                currObj.addClass('yellow');
+            }
+            else {
+                currObj.addClass('green');
+            }
+        },
+        error: function() {
+        alert('Failed')
+        }
+
+
+    });
+
+    }
 
 
 })
